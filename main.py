a77d16d2477c128247f4e4743c282c9a351da899
dgsiilvaa020-cmyk <dg.siilvaa020@gmail.com>
2026-07-31 23:29:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2813,6 +2813,8 @@ async def editar_autor_livro(callback: CallbackQuery):
 @dp.callback_query(F.data.startswith("atualizar_livro_"))
 async def atualizar_livro_acervo(callback: CallbackQuery):
 
+     print("ENTROU NO ATUALIZAR LIVRO")
+
     if not autorizado(callback.from_user.id):
         return
 
